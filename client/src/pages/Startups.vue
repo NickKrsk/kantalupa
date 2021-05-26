e59d3f0db947cb3ebec5e788c49d804d2257220e
sergey <serge.dmitriev@gmail.com>
2021-05-26 13:56:31 +0700

    fix

---
+++ client/src/pages/Startups.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    Startups
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Startups',
+};
+</script>
--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -7,6 +7,7 @@ const routes = [
       { path: 'login', component: () => import('pages/Login.vue') },
       { path: 'register', component: () => import('pages/Register.vue') },
       { path: 'corps', component: () => import('pages/Corps.vue') },
+      { path: 'startups', component: () => import('pages/Startups.vue') },
     ],
   },
 
