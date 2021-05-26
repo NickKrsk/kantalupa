07ed6ff6904f16da43353ad01bbf0c2045b0239d
sergey <serge.dmitriev@gmail.com>
2021-05-26 12:12:12 +0700

    fix

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,14 +1,62 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
-</template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-between">
 
-<script>
-export default {
-  name: 'PageIndex',
-};
-</script>
+      <q-parallax
+        src="https://cdn24.img.ria.ru/images/149555/56/1495555668_19:0:10019:5625_600x0_80_0_0_f3109c9d45d4b1518f2e528c4838daff.jpg"
+      >
+        <h1 class="text-white">Место встречи инноваций и бизнеса</h1>
+      </q-parallax>
+
+    </div>
+    <div class="row">
+      <div class="col q-pa-md">
+        <q-btn color="primary" class="full-width" label="Найти инвестиции" />
+      </div>
+      <div class="col q-pa-md">
+        <q-btn color="secondary" class="full-width" label="Выбрать стартап" />
+      </div>
+    </div>
+
+    <div>
+      <h4>
+        Сообщество инноваций
+      </h4>
+      <p>
+        Помощь коллег, разработчиков, внедренцев и интеграторов для реализации комплексных проектов.
+        Проверенные решения для повседневных задач и обсуждение эксклюзивных идей. Присоединяйся!
+      </p>
+    </div>
+
+     <div class="row">
+      <div class="col q-pa-md">
+        <h4>Для стартапов</h4>
+        <ul>
+          <li>Найти инвестиции</li>
+          <li>Обсудить идеи</li>
+          <li>Найти единомышленников, сотрудников</li>
+        </ul>
+        <q-btn color="primary" class="full-width" label="Зарегистрироваться" />
+      </div>
+      <div class="col q-pa-md">
+        <h4>Для крупного бизнеса</h4>
+        <ul>
+          <li>Найти решения</li>
+          <li>Найти подрядчиков</li>
+          <li>Найти инновации</li>
+        </ul>
+        <q-btn color="secondary" class="full-width" label="Предложить проблему" />
+      </div>
+      <div class="col q-pa-md">
+        <h4>Экосистема</h4>
+        <ul>
+          <li>Предложить инвестиции</li>
+          <li>Предложить ресурсы</li>
+          <li>Участвовать в развитии</li>
+        </ul>
+        <q-btn color="secondary" class="full-width" label="Присоединиться" />
+      </div>
+    </div>
+  </div>
+
+</template>
