3cf44b5ecc848afe4428475d1e5b793073a863d9
NickKrsk <ggd43@rambler.ru>
2021-05-26 12:16:05 +0700

    f

--- client/src/pages/Corps.vue
+++ client/src/pages/Corps.vue
@@ -1,20 +1,58 @@
 <template>
-  <div class="q-pa-md" style="max-width: 350px">
-    <q-list bordered separator>
-     <q-item 
-      v-for="item in allCorps"
-      :key="item.id"
-      :label="item.name"
-      clickable 
-      v-ripple     
-      @click="selectCorp(group)"        
-     >
+  <div class="row q-pa-md" style="max-width: 350px">
+    <div class="col-5 q-pa-sm">
+      Корпорации
+      <q-list bordered separator>
+        <q-item 
+          v-for="item in allCorps"
+          :key="item.id"
+          :label="item.name"
+          clickable 
+          v-ripple     
+          @click="selectCorp(item)"        
+        >
         <q-item-section>
-          <q-item-label>{{item.name}}</q-item-label>
-          <q-item-label caption>Caption</q-item-label>
+          <q-item-label>{{item.name}}</q-item-label>          
         </q-item-section>
-      </q-item>
-    </q-list>
+        </q-item>
+      </q-list>
+    </div>
+
+    <div class="col-5 q-pa-sm">
+      Запросы
+      <q-list bordered separator v-if="filtredReq.length > 0">
+        <q-item 
+          v-for="item in filtredReq"
+          :key="item.id"
+          :label="item.desc"
+          clickable 
+          v-ripple     
+          @click="selectCorp(group)"        
+        >
+        <q-item-section>
+          <q-item-label>{{item.desc}}</q-item-label>
+        </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+
+    <div class="col-5 q-pa-sm">
+      Стартапы
+      <q-list bordered separator>
+        <q-item 
+          v-for="item in startups"
+          :key="item.startupId"
+          :label="item.name"
+          clickable 
+          v-ripple     
+          @click="selectCorp(item)"        
+        >
+        <q-item-section>
+          <q-item-label>{{item.name}}</q-item-label>          
+        </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </div>
 </template>
 
@@ -38,19 +76,49 @@ export default {
       allCorps: [
         {
           name: "RusAll",
-          id: 1,
+          corpId: 1,
         },
         {
           name: "RusAll123",
-          id: 2,
+          corpId: 2,
+        },
+      ],      
+      allRequests: [
+        {
+          desc: "Создать станок",
+          corpId: 1,
+        },
+        {
+          desc: "Создать технолгию",
+          corpId: 2,
         },
       ],
+
+      startups: [
+        {
+          name: "Стартап1",
+          startupId: 1,
+        },
+        {
+          name: "Стартап2",
+          startupId: 2,
+        },
+        {
+          name: "Стартап3",
+          startupId: 3,
+        },
+        {
+          name: "Стартап4",
+          startupId: 4,
+        },
+      ],  
+      filtredReq: [],
     };
   },
   methods: {   
-    selectGroup(group) {
-      this.currentGroup = group;
-      this.onGroupSelect(group);
+    selectCorp(corp) {
+      this.filtredReq = this.allRequests.filter((el) => el.corpId === corp.corpId);   
+      console.log(this.filtredReq);   
     },
   },
   computed: {
